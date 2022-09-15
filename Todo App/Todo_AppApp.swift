5fba3e1cf29b58d3e4921b87e36a00784033706f
Hayato Watanabe <solideye@me.com>
2022-09-15 12:09:48 +0900

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Hayato Watanabe on 2022/09/15.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
