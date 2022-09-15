10f197a99fc9e104e01c191aea41dd86591a1a6d
Hayato Watanabe <solideye@me.com>
2022-09-15 13:50:02 +0900

    Add ContentView

--- Todo App/ContentView.swift
+++ Todo App/ContentView.swift
@@ -7,10 +7,69 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View, InputViewDelegate {
+    @State var todos: [String] = []
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack(alignment: .bottomTrailing) {
+                List {
+                    ForEach(todos, id: \.self) { user in
+                        Text(user)
+                    }
+                    .onDelete(perform: delete)
+                }
+                
+                NavigationLink(destination: InputView(delegate: self, text: "")) {
+                    Text("Add")
+                        .foregroundColor(.white)
+                        .font(.system(size: 20))
+                }
+                .frame(width: 60, height: 60)
+                .background(.orange)
+                .cornerRadius(30)
+                .padding()
+            }
+            .onAppear {
+                if let todos = UserDefaults.standard.array(forKey: "TODO") as? [String] {
+                    self.todos = todos
+                }
+            }
+            .navigationTitle("TODO")
+            .navigationBarItems(trailing: EditButton())
+        }
+    }
+    
+    func delete(at offsets: IndexSet) {
+        todos.remove(atOffsets: offsets)
+        print(todos)
+        UserDefaults.standard.setValue(todos, forKey: "TODO")
+    }
+    
+    func addTodo(text: String) {
+        todos.append(text)
+        UserDefaults.standard.setValue(todos, forKey: "TODO")
+    }
+}
+
+protocol InputViewDelegate {
+    func addTodo(text: String)
+}
+
+struct InputView: View {
+    @Environment(\.presentationMode) var presentation
+    let delegate: InputViewDelegate
+    @State var text: String
+    var body: some View {
+        VStack(spacing: 16) {
+            TextField("Input your TODO", text: $text)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Button("Add") {
+                delegate.addTodo(text: text)
+                presentation.wrappedValue.dismiss()
+            }
+        }
+        .padding()
     }
 }
 
